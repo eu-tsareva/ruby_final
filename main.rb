9d2f1cb91289eb987462c793702cfc5091bc6fb7
tsareva <e.tsareva@uit.space>
2019-02-26 13:11:25 +0300

    add game loop

--- main.rb
+++ main.rb
@@ -3,3 +3,23 @@ require_relative 'deck.rb'
 require_relative 'player.rb'
 require_relative 'user.rb'
 require_relative 'dealer.rb'
+require_relative 'game.rb'
+
+print 'Type your name: '
+name = gets.chomp
+loop do
+  game = Game.new(name)
+  loop do
+    puts "Game on!\n"
+    game.start
+    game.play while game.on?
+    game.finish
+    break unless game.continue?
+
+    puts "#{name}, would you like to play again? (y/n)"
+    break if gets.chomp == 'n'
+  end
+  puts "One of the players has no money"
+  puts "#{name}, would you like to restart? (y/n)"
+  break if gets.chomp == 'n'
+end
